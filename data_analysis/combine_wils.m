2b941c02f97c9cad1ec7d1acfd24521cca2264b9
Joshua S <joshuatsdev@gmail.com>
2022-01-20 11:05:27 -0500

    Calculate string tension with error bars

--- data_analysis/combine_wils.m
+++ data_analysis/combine_wils.m
@@ -1,11 +1,9 @@
 wils_dt_4={};
 wils_dt_5={};
 
-n=length(wils_dt1)/2;
+n=5;
 for i=1:n
-    wils_dt_4{i}=[wils_dt1{i}; wils_dt2{i}; wils_dt3{i}];
-end
-
-for i=(n+1):length(wils_dt1)
-    wils_dt_5{i-n}=[wils_dt1{i}; wils_dt2{i}; wils_dt3{i}];
+    wils_dt_3{i}=[wils_11{3}{i}; wils_12{3}{i}; wils_13{3}{i}; wils_14{3}{i}; wils_15{3}{i};];
+    wils_dt_4{i}=[wils_11{4}{i}; wils_12{4}{i}; wils_13{4}{i}; wils_14{4}{i}; wils_15{4}{i}; wils_16{4}{i}; wils_17{4}{i}; wils_18{4}{i};];
+    wils_dt_5{i}=[wils_11{5}{i}; wils_12{5}{i}; wils_13{5}{i}; wils_14{5}{i}; wils_15{5}{i}; wils_16{5}{i}; wils_17{5}{i}; wils_18{5}{i};];
 end
--- data_analysis/jacknife.m
+++ data_analysis/jacknife.m
@@ -1,4 +1,4 @@
-function [V,errs] = jacknife(wils_dt1,wils_dt2)
+function [V,errs,st,st_err] = jacknife(wils_dt1,wils_dt2)
 
 Ndists=length(wils_dt1);
 wil=wils_dt1(1);
@@ -6,17 +6,27 @@ wil=wil{1};
 N=length(wil);
 
 V=get_potential(wils_dt1,wils_dt2,0);
+%st=string_tension(mean([V(3)-V(2),V(4)-V(3)]),2.922);
+st=string_tension(V(4)-V(3),2.922);
 
 errs=zeros(Ndists,1);
+st_err=0;
 bias=zeros(Ndists,1);
+st_bias=0;
 for i=1:N
     V_omit=get_potential(wils_dt1,wils_dt2,i);
+    %st_omit=string_tension(mean([V_omit(3)-V_omit(2),V_omit(4)-V_omit(3)]),2.922);
+    st_omit=string_tension(V_omit(4)-V_omit(3),2.922);
+    st_err=st_err+(N-1)/N*(st-st_omit)^2;
     errs=errs+(N-1)/N*(V-V_omit).^2;
     bias=bias+V_omit/N;
+    st_bias=st_bias+st_omit/N;
 end
 
 errs=errs.^0.5;
+st_err=st_err^0.5;
 V=V-(N-1)*(bias-V);
+st=st-(N-1)*(st_bias-st);
 
 end
 
