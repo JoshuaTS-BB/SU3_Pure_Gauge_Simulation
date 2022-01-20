37344d017a9d1a47571218c0ee4765c835988c36
Joshua S <joshuatsdev@gmail.com>
2022-01-20 11:06:23 -0500

    Update wilson loop script

--- scripts/calc_wilson_loops.m
+++ scripts/calc_wilson_loops.m
@@ -1,26 +1,32 @@
+function wils_dt = calc_wilson_loops(filename)
+
 %num_proc=str2num(getenv('SLURM_NTASKS'));
 %pool=parpool('local',num_proc);
 
-filename="ensemble_12x12_beta_5-7_cold_09-Nov-2021_1";
-load("ensembles/"+filename+".mat");
-ensemble2(:,:,:,:,:,:,:)=ensemble(5:5:500,:,:,:,:,:,:);
+%filename="ensemble_12x12_beta_5-7_cold_18-Jan-2022_14";
+load("ensembles/12x12/"+filename+".mat","ensemble");
+ensemble2(:,:,:,:,:,:,:)=ensemble(10:30:150,:,:,:,:,:,:);
+clear ensemble
 
 addpath("wilson_loops");
 
-dts=4:5;
-Ndt=length(dts);
-Ndx=7;
+dti=4;
+Ndt=2;
+Ndx=5;
 
 wils_dt=cell(1,Ndx*Ndt);
-save("wilson_loops/wilson_loops_"+filename+".mat","wils_dt","Ndx","dts")
+save("wilson_loops/wilson_loops_"+filename+".mat","wils_dt","Ndx","dti")
 
 parfor i=1:Ndt*Ndx
     dx=mod(i,Ndx);
     if(dx==0) dx=Ndx; end
-    dt=floor(i/Ndx)+1;
+    dt=floor((i-1)/Ndx)+dti;
+    [dx,dt]
     wils_dt(i)={plot_wilson(ensemble2,dx,dt)};
 end
 
-save("wilson_loops/wilson_loops_"+filename+".mat","wils_dt","Ndx","dts")
+save("wilson_loops/wilson_loops_"+filename+".mat","wils_dt","Ndx","dti")
 
-delete(pool);
+%delete(pool);
+
+end
