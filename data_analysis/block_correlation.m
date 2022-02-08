5cca73f52395420b83220e923d2047e99dc0a5c5
Joshua S <joshuatsdev@gmail.com>
2022-02-08 11:06:31 -0500

    Updates data analysis

--- data_analysis/block_correlation.m
+++ data_analysis/block_correlation.m
@@ -1,8 +1,8 @@
-function stds = block_correlation(v,Np,intersection)
+function vars = block_correlation(v,Np,intersection)
 
 N=size(v,1);
-stds=zeros(floor(N/2),1);
-stds(1)=var(v);
+vars=zeros(floor(N/2),1);
+vars(1)=var(v);
 
 for K=2:floor(N/2)
     l=floor(N/K);
@@ -10,14 +10,14 @@ for K=2:floor(N/2)
     for i=1:l
         av(i)=mean(v((i-1)*K+1:i*K));
     end
-    stds(K)=var(av);
+    vars(K)=var(av);
 end
 
 K_inv=(1:Np).^(-1);
-K_inv=K_inv.*(stds(intersection)/K_inv(intersection));
+K_inv=K_inv.*(vars(intersection)/K_inv(intersection));
 
 hold on;
-plot(stds(1:Np));
+plot(vars(1:Np));
 plot(K_inv(1:Np));
 hold off;
 
--- data_analysis/combine_wils.m
+++ data_analysis/combine_wils.m
@@ -4,6 +4,14 @@ wils_dt_5={};
 n=5;
 for i=1:n
     wils_dt_3{i}=[wils_11{3}{i}; wils_12{3}{i}; wils_13{3}{i}; wils_14{3}{i}; wils_15{3}{i};];
-    wils_dt_4{i}=[wils_11{4}{i}; wils_12{4}{i}; wils_13{4}{i}; wils_14{4}{i}; wils_15{4}{i}; wils_16{4}{i}; wils_17{4}{i}; wils_18{4}{i};];
-    wils_dt_5{i}=[wils_11{5}{i}; wils_12{5}{i}; wils_13{5}{i}; wils_14{5}{i}; wils_15{5}{i}; wils_16{5}{i}; wils_17{5}{i}; wils_18{5}{i};];
+    wils_dt_4{i}=[wils_11{4}{i}; wils_12{4}{i}; wils_13{4}{i}; wils_14{4}{i}; wils_15{4}{i}; wils_16{4}{i}; wils_17{4}{i}; wils_18{4}{i};
+        wils_20{4}{i}; wils_21{4}{i}; wils_22{4}{i}; wils_23{4}{i}; wils_24{4}{i}; wils_25{4}{i}; wils_26{4}{i}; wils_27{4}{i}; 
+        wils_30{4}{i}; wils_31{4}{i}; wils_32{4}{i}; wils_33{4}{i}; wils_34{4}{i}; wils_35{4}{i}; wils_36{4}{i}; wils_37{4}{i};
+        wils_40{4}{i}; wils_41{4}{i}; wils_42{4}{i}; wils_43{4}{i}; wils_44{4}{i}; wils_45{4}{i}; wils_46{4}{i}; wils_47{4}{i};
+        wils_50{4}{i}; wils_51{4}{i}; wils_52{4}{i}; wils_53{4}{i}; wils_54{4}{i}; wils_55{4}{i}; wils_56{4}{i}; wils_57{4}{i}; wils_58{4}{i};];
+    wils_dt_5{i}=[wils_11{5}{i}; wils_12{5}{i}; wils_13{5}{i}; wils_14{5}{i}; wils_15{5}{i}; wils_16{5}{i}; wils_17{5}{i}; wils_18{5}{i};
+        wils_20{5}{i}; wils_21{5}{i}; wils_22{5}{i}; wils_23{5}{i}; wils_24{5}{i}; wils_25{5}{i}; wils_26{5}{i}; wils_27{5}{i};
+        wils_30{5}{i}; wils_31{5}{i}; wils_32{5}{i}; wils_33{5}{i}; wils_34{5}{i}; wils_35{5}{i}; wils_36{5}{i}; wils_37{5}{i};
+        wils_40{5}{i}; wils_41{5}{i}; wils_42{5}{i}; wils_43{5}{i}; wils_44{5}{i}; wils_45{5}{i}; wils_46{5}{i}; wils_47{5}{i};
+        wils_50{5}{i}; wils_51{5}{i}; wils_52{5}{i}; wils_53{5}{i}; wils_54{5}{i}; wils_55{5}{i}; wils_56{5}{i}; wils_57{5}{i}; wils_58{5}{i};];
 end
