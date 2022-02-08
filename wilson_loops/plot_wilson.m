4f52521618f53e5d34e822538f06444f6d2f1d06
Joshua S <joshuatsdev@gmail.com>
2022-02-08 11:07:52 -0500

    Added more information to output

--- wilson_loops/plot_wilson.m
+++ wilson_loops/plot_wilson.m
@@ -1,11 +1,11 @@
-function p = plot_wilson(ensemble,dx,dt)
+function p = plot_wilson(ensemble,dx,dt,idx)
 
 N=size(ensemble);
 p=zeros(N(1),1);
 
 for i=1:N(1)
-    i
-    w=wilson_loop_su3(ensemble(i,:,:,:,:,:,:),N(4),N(5),dx,dt)
+    [idx, dx, dt, i]
+    w=wilson_loop_su3(ensemble(i,:,:,:,:,:,:),N(4),N(5),dx,dt);
     p(i)=w;
 end
 
